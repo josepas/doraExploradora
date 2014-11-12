0d0d3c1678905f5d4e498146d5563a800c7e9689
Francisco Martinez <frammnm@gmail.com>
2014-11-12 16:05:58 -0430

    Agregado botas, tips y modificaciones en carpetas.py

---
+++ botas.py
@@ -0,0 +1,27 @@
+
+import sys
+
+
+usage = " Es necesario darle un solo argumento a botas, y debe ser "
+usage += "del estilo :\n [1-4].[1-9]  \n"
+
+
+if (len(sys.argv) == 2 ):
+    npreg =sys.argv[1]
+else:
+    print usage 
+    exit(1)
+
+f = open('tips.txt','r') 
+
+
+conseguido = False
+
+for line in f:    
+    preg =line.split()
+    if (preg[0] == npreg) :  
+        print preg[1]
+        conseguido = True
+
+if not conseguido : 
+    print "No tengo consejos para esta pregunta" 
--- carpetas.py
+++ carpetas.py
@@ -5,11 +5,16 @@ import os
 
 #Se crea la carpeta contenedora de todo
 
-os.system('mkdir --mode=755 /home/invitado/Tarea3 ')
+os.system('mkdir --mode=755 /home/invitado/Tarea3')
+os.system('cp recursos/tips.txt /home/invitado/Tarea3')
+os.system('cp botas.py  /home/invitado/Tarea3')
+os.system('mv /home/invitado/Tarea3/tips.txt /home/invitado/Tarea3/.tips.txt')
+
 
 #Se crea la mochila 
 
 os.system('mkdir --mode=766 /home/invitado/Tarea3/mochila' ) 
+
 
 #Se crea cada una de las localidades en el mundo de dora
 
@@ -21,6 +26,8 @@ os.system('mkdir --mode=000 /home/invitado/Tarea3/CuevaDelChaMAC')
 print 'Se crean la mochila, localidades, Dir Raiz'
 
 os.system('alias manpa=man')
+os.system('alias botas="python botas.py"')
 
 print 'Alias de manpa' 
+
 
