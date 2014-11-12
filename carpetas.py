d97d323eed52ad3013660ac13ea29438cdedbf8d
Francisco Martinez <frammnm@gmail.com>
2014-11-12 14:49:29 -0430

    Agregado el script de la creacion de carpetas y directorio padre

---
+++ carpetas.py
@@ -0,0 +1,26 @@
+import os
+
+
+
+
+#Se crea la carpeta contenedora de todo
+
+os.system('mkdir --mode=755 /home/invitado/Tarea3 ')
+
+#Se crea la mochila 
+
+os.system('mkdir --mode=766 /home/invitado/Tarea3/mochila' ) 
+
+#Se crea cada una de las localidades en el mundo de dora
+
+os.system('mkdir --mode=000 /home/invitado/Tarea3/LagoDubi')
+os.system('mkdir --mode=000 /home/invitado/Tarea3/ElMonteSino')
+os.system('mkdir --mode=000 /home/invitado/Tarea3/DesiertoDelCharara')
+os.system('mkdir --mode=000 /home/invitado/Tarea3/CuevaDelChaMAC')
+
+print 'Se crean la mochila, localidades, Dir Raiz'
+
+os.system('alias manpa=man')
+
+print 'Alias de manpa' 
+
