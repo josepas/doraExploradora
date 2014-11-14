4cedd7c4bb0265ef9d7e7cb73ff8ae0af7afd6b5
Melecio Ponte <pontezambrano@gmail.com>
2014-11-14 15:28:15 -0430

    Another stash

--- botas.py
+++ botas.py
@@ -4,9 +4,12 @@ import os
 import signal
 import time
 
+
+os.system("cat ascciDora/botas")
+
 def receive_signal(signum, stack):
     print '-----> No puedes matar a boticas tontito :) \n'
- 
+
 
 usage = " Es necesario darle un solo argumento a botas, y debe ser "
 usage += "del estilo :\n [1-4].[1-9]  \n"
@@ -15,27 +18,27 @@ usage += "del estilo :\n [1-4].[1-9]  \n"
 if (len(sys.argv) == 2 ):
     npreg =sys.argv[1]
 else:
-    print usage 
+    print usage
     exit(1)
 
 
 
-f = open('tips.txt','r') 
+f = open('tips.txt','r')
 
-    
+
 conseguido = False
 
-for line in f:    
+for line in f:
     preg =line.split()
-    if (preg[0] == npreg) :  
+    if (preg[0] == npreg) :
         print "Consejo: \n"
         print preg[1]
         print "\n"
         conseguido = True
 
-if not conseguido : 
-        print "No tengo consejos para esta pregunta \n\n" 
-    
+if not conseguido :
+        print "No tengo consejos para esta pregunta \n\n"
+
 print "botas se fue por tu preguntadera \n"
 
 signal.signal(signal.SIGINT, receive_signal)
